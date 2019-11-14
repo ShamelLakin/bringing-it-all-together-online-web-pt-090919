4d2c55cea582b181973290dff97bba2f506759aa
Shamel Lakin <christlife4u@gmail.com>
2019-11-14 17:28:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :id, :name, :breed 
   
   def initialize(id=nil name:, breed:)
-    @name = name 
-    @breed = breed 
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
   end 
 end 
